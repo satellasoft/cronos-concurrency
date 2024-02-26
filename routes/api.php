cef30fbe562e0fe594efdf3e97f25539ee984af9
SatellaSoft <=>
2024-02-26 08:47:56 -0300

    feat: remove lines test

--- routes/api.php
+++ routes/api.php
@@ -2,20 +2,9 @@
 
 use App\Http\Controllers\CustomerController;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Log;
 
 Route::prefix('customer')->group(function () {
     Route::post('/', [CustomerController::class, 'store'])->name('customer.store');
     Route::get('/{id}', [CustomerController::class, 'getBalance'])->name('customer.get_balance')->whereNumber('id');
     Route::put('/{id}/update-balance', [CustomerController::class, 'updateBalance'])->name('customer.update')->whereNumber('id');
-});
-
-Route::get('info', function () {
-    phpinfo();
-});
-
-Route::get('log', function () {
-    Log::warning('Testing Log');
-
-    dd('logged');
 });
